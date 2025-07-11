ac486b20626cb04ac36c21c5c72ae8d30910bbc8
issath-voisek <issath@voisek.com>
2025-07-12 03:04:37 +0530

    Initial commit

---
+++ myApp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from chat import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('chat/<str:room_name>/', views.room, name='room'),
+]
